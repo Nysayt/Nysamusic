2eae38ad2ed13a06e9e491385db448d59f97755f
rockyxd3 <shivamlkr629@gmail.com>
2026-05-26 17:09:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,11 +22,11 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/fallenx")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/mecobots")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/mecochat")
 
-        self.API_URL = getenv("API_URL", "https://api.fallenapi.fun")
-        self.API_KEY = getenv("API_KEY", None) # Get this value from @FallenApiBot by sending /apikey
+        self.API_URL = getenv("API_URL", "https://api.onegrab.fun")
+        self.API_KEY = getenv("API_KEY", "fbee25_3PnwnjdSUL3KGPNESb5tmApWWhfSgm4y") # Get this value from @FallenApiBot by sending /apikey
 
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", "False").lower() == "true"
         self.AUTO_END: bool = getenv("AUTO_END", "False").lower() == "true"
@@ -42,7 +42,7 @@ class Config:
         ]
         self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
         self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/haagg2.png")
-        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/zvziwk.jpg")
+        self.START_IMG = getenv("START_IMG", "https://graph.org/file/0d3f1affae6417ae16516-fe3d1b4a2f192fd9a2.jpg")
 
     def check(self):
         missing = [
