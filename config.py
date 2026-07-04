340c2cab275d671bb66de23ee116fee20c3160a5
rockyxd3 <shivamlkr629@gmail.com>
2026-07-04 15:05:23 +0530

    Update SUPPORT_CHANNEL URL in config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TeamAuro")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/rossy_music_world")
         self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Aurosupport")
 
         self.API_URL = getenv("API_URL", "https://api.onegrab.fun")
