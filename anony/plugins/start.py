e711f0f074268c90c2b3835e22d89d5f7cb0558e
rockyxd3 <shivamlkr629@gmail.com>
2026-07-04 06:37:12 +0530

    Update start.py

--- anony/plugins/start.py
+++ anony/plugins/start.py
@@ -103,13 +103,13 @@ async def _new_member(_, message: types.Message):
                             text=message.lang["add_me"],
                             url=f"https://t.me/{app.username}?startgroup=true",
                             style=ButtonStyle.PRIMARY,
-                        )
+                        ),
                         types.InlineKeyboardButton(
                             text=message.lang["support"],
                             url=config.SUPPORT_CHAT,
                             style=ButtonStyle.SUCCESS,
-                        )
-                    ],
+                        ),
+                    ]
                 ]
             )
             try:
